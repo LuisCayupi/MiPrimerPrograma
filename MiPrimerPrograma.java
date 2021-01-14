3211535c2e014aaebbe99a2cd94b66f978357be1
Luis Cayupi <lptcayu@gmail.com>
2021-01-14 13:14:40 -0300

    mi primer programa

---
+++ MiPrimerPrograma.java
@@ -0,0 +1,11 @@
+public class MiPrimerPrograma {
+
+	public static void main(String[] args) {
+		int edad=23;
+		String nombre="Luis Cayupi";
+		String ciudad="Temuco";
+		System.out.println("Mi nombre es " + nombre +".");
+		System.out.println("Tengo "+ edad +" años de edad.");
+		System.out.println("Mi ciudad es " + ciudad +".");
+	}
+}
